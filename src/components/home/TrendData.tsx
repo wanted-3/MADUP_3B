a2eb3a756ff6cff286ad3b6a6d8d7f94c20fb06c
leejh4197 <leejh4197@naver.com>
2022-05-23 14:20:35 +0900

    feat:css

--- src/components/home/TrendData.tsx
+++ src/components/home/TrendData.tsx
@@ -23,74 +23,74 @@ const TrendData = () => {
             <span>(CVR)</span>
           </div>
           <div className={styles.snsReports}>
-            <p className={styles.faceBookTitle}>페이스북</p>
-            <p>광고비</p>
-            <p>매출</p>
-            <p>ROAS</p>
-            <p>노출수</p>
-            <p>클릭 수</p>
-            <p>클릭률 (CTR)</p>
-            <p>클릭당비용 (CPC)</p>
-            <p>(CPA)</p>
-            <p>DATE</p>
-            <p>COST</p>
-            <p>(CVR)</p>
+            <span className={styles.snsTitle}>페이스북</span>
+            <span>광고비</span>
+            <span>매출</span>
+            <span>ROAS</span>
+            <span>노출수</span>
+            <span>클릭 수</span>
+            <span>클릭률 (CTR)</span>
+            <span>클릭당비용 (CPC)</span>
+            <span>(CPA)</span>
+            <span>DATE</span>
+            <span>COST</span>
+            <span>(CVR)</span>
           </div>
           <div className={styles.snsReports}>
-            <p className={styles.naverTitle}>네이버</p>
-            <p>광고비</p>
-            <p>매출</p>
-            <p>ROAS</p>
-            <p>노출수</p>
-            <p>클릭 수</p>
-            <p>클릭률 (CTR)</p>
-            <p>클릭당비용 (CPC)</p>
-            <p>(CPA)</p>
-            <p>DATE</p>
-            <p>COST</p>
-            <p>(CVR)</p>
+            <span className={styles.snsTitle}>네이버</span>
+            <span>광고비</span>
+            <span>매출</span>
+            <span>ROAS</span>
+            <span>노출수</span>
+            <span>클릭 수</span>
+            <span>클릭률 (CTR)</span>
+            <span>클릭당비용 (CPC)</span>
+            <span>(CPA)</span>
+            <span>DATE</span>
+            <span>COST</span>
+            <span>(CVR)</span>
           </div>
           <div className={styles.snsReports}>
-            <p className={styles.snsTitle}>구글</p>
-            <p>광고비</p>
-            <p>매출</p>
-            <p>ROAS</p>
-            <p>노출수</p>
-            <p>클릭 수</p>
-            <p>클릭률 (CTR)</p>
-            <p>클릭당비용 (CPC)</p>
-            <p>(CPA)</p>
-            <p>DATE</p>
-            <p>COST</p>
-            <p>(CVR)</p>
+            <span className={styles.snsTitle}>구글</span>
+            <span>광고비</span>
+            <span>매출</span>
+            <span>ROAS</span>
+            <span>노출수</span>
+            <span>클릭 수</span>
+            <span>클릭률 (CTR)</span>
+            <span>클릭당비용 (CPC)</span>
+            <span>(CPA)</span>
+            <span>DATE</span>
+            <span>COST</span>
+            <span>(CVR)</span>
           </div>
           <div className={styles.snsReports}>
-            <p className={styles.snsTitle}>카톡</p>
-            <p>광고비</p>
-            <p>매출</p>
-            <p>ROAS</p>
-            <p>노출수</p>
-            <p>클릭 수</p>
-            <p>클릭률 (CTR)</p>
-            <p>클릭당비용 (CPC)</p>
-            <p>(CPA)</p>
-            <p>DATE</p>
-            <p>COST</p>
-            <p>(CVR)</p>
+            <span className={styles.snsTitle}>카톡</span>
+            <span>광고비</span>
+            <span>매출</span>
+            <span>ROAS</span>
+            <span>노출수</span>
+            <span>클릭 수</span>
+            <span>클릭률 (CTR)</span>
+            <span>클릭당비용 (CPC)</span>
+            <span>(CPA)</span>
+            <span>DATE</span>
+            <span>COST</span>
+            <span>(CVR)</span>
           </div>
           <div className={styles.totalReport}>
-            <p className={styles.snsTitle}>총계</p>
-            <p>광고비</p>
-            <p>매출</p>
-            <p>ROAS</p>
-            <p>노출수</p>
-            <p>클릭 수</p>
-            <p>클릭률 (CTR)</p>
-            <p>클릭당비용 (CPC)</p>
-            <p>(CPA)</p>
-            <p>DATE</p>
-            <p>COST</p>
-            <p>(CVR)</p>
+            <span className={styles.snsTitle}>총계</span>
+            <span>광고비</span>
+            <span>매출</span>
+            <span>ROAS</span>
+            <span>노출수</span>
+            <span>클릭 수</span>
+            <span>클릭률 (CTR)</span>
+            <span>클릭당비용 (CPC)</span>
+            <span>(CPA)</span>
+            <span>DATE</span>
+            <span>COST</span>
+            <span>(CVR)</span>
           </div>
         </div>
       </div>

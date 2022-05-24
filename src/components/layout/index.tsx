01156ecbd596ac41ff9ca3cca2141830bb1c59d8
kimmsoll <kimmsoll@naver.com>
2022-05-24 17:07:03 +0900

    feat: create header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import styles from './layout.module.scss'
+import { BellIcon, SettingIcon, UserIcon } from 'assets/svgs'
 
 const Layout = () => {
   return (
     <div className={styles.appLayout}>
       <Navbar />
-      <main>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <BellIcon className={styles.bell} />
+          <SettingIcon className={styles.setting} />
+          <div className={styles.user}>
+            <UserIcon />
+            <span>3B팀 님</span>
+          </div>
+        </header>
         <Outlet />
       </main>
     </div>
